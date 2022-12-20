1f6eecf46ad907d0e885944c79042fdb7547bee7
svub <svub@no.email>
2022-12-20 18:29:11 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'stay.in.touch',
+  appName: 'stay-in-touch',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
