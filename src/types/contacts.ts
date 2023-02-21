2e1cd2490baa97e073d0bf06b0bbbb801c5b1e16
svub <svub@no.email>
2023-02-21 12:43:23 +0100

    typing for contacts

---
+++ src/types/contacts.ts
@@ -0,0 +1,38 @@
+export enum LocationPrecision {
+  exact, city, state, country, continent,
+}
+
+export type ContactLocation = {
+  precision: LocationPrecision;
+  label: string;
+  lat?: number;
+  lng?: number;
+  countryCode?: string;
+}
+
+export type ContactProfile = {
+  name: string;
+  label?: string;
+  avatar: string;
+  location: ContactLocation;
+}
+
+export type ContactDataItem = {
+  key: string;
+  value: string;
+}
+
+export type ContactUrlItem = {
+  label: string;
+  url: string;
+}
+
+export type Contact = {
+  id: string; // address or pub?
+  pub: JsonWebKey;
+  profile: ContactProfile;
+  data: ContactDataItem[];
+  urls: ContactUrlItem[];
+  sources: string[];
+};
+
