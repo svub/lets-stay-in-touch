1af43e2ccad037bb372d3d16cae5682a6becb388
svub <svub@no.email>
2023-03-14 19:50:03 +0100

    keeping app state in local storage

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import App from './App.vue';
 import router from './router';
 
 import { IonicVue } from '@ionic/vue';
-import { createPinia } from 'pinia';
+import { createPinia, Store } from 'pinia';
+import localForage from "localforage";
+import { debounce } from "lodash";
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -24,10 +26,45 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+localForage.config({
+  name        : 'lsit',
+  version     : 1.0,
+  storeName   : 'pinia_backup',
+});
+
+// TODO: consider extracting this into generic module, needed this functionality in two projects already.
+async function persistencePlugin({ store }: { store: Store }) {
+  const key = store.$id + '-state';
+
+  let quick = localStorage.getItem(key)
+  if (quick) {
+    try {
+      store.$patch(JSON.parse(quick))
+    } catch(e) {
+      quick = null; // trigger patching via localForage
+    }
+  }
+
+  const stored = await localForage.getItem(key) as string;
+  if (stored && quick != stored) {
+      store.$patch(JSON.parse(stored))
+      localStorage.setItem(key, stored);
+  }
+
+  store.$subscribe(debounce(() => {
+    const data = JSON.stringify(store.$state); // plain objects should work but caused problems, to be tested, but serialized works always.
+    localStorage.setItem(key, data);
+    localForage.setItem(key, data);
+  }, 50));
+}
+
+const pinia = createPinia();
+pinia.use(persistencePlugin);
+
 const app = createApp(App)
     .use(IonicVue)
     .use(router)
-    .use(createPinia());
+    .use(pinia);
 
 router.isReady().then(() => {
   app.mount('#app');
